8bf78eedac88a549694284e246888e89630a8102
Anderson Irias <anderson.irias@gmail.com>
2018-12-30 22:17:13 -0200

    Função editar usuário

--- server.js
+++ server.js
@@ -65,3 +65,27 @@ app.post('/api', function(req, res){
   });
 });
 
+app.put('/api', function(req, res){
+  fs.readFile('usuarios.json', 'utf8', function(err, data){
+    if (err) {
+      var response = {status: 'falha', resultado: err};
+      res.json(response);
+    } else {
+      var obj = JSON.parse(data);
+
+      obj.usuarios[(req.body.usuario_id - 1)].nome = req.body.nome;
+      obj.usuarios[(req.body.usuario_id - 1)].site = req.body.site;
+
+      fs.writeFile('usuarios.json', JSON.stringify(obj), function(err) {
+        if (err) {
+          var response = {status: 'falha', resultado: err};
+          res.json(response);
+        } else {
+          var response = {status: 'sucesso', resultado: 'Registro editado com sucesso'};
+          res.json(response);
+        }
+      });
+    }
+  });
+});
+
