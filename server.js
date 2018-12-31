6a0991ce75231d0fd1cecc79f5f4cfdc343451cb
Anderson Irias <anderson.irias@gmail.com>
2018-12-30 22:16:31 -0200

    Função inserir usuário

--- server.js
+++ server.js
@@ -41,4 +41,27 @@ app.get('/api', function(req, res){
   });
 });
 
+app.post('/api', function(req, res){
+  fs.readFile('usuarios.json', 'utf8', function(err, data){
+    if (err) {
+      var response = {status: 'falha', resultado: err};
+      res.json(response);
+    } else {
+      var obj = JSON.parse(data);
+      req.body.usuario_id = obj.usuarios.length + 1;
+
+      obj.usuarios.push(req.body);
+
+      fs.writeFile('usuarios.json', JSON.stringify(obj), function(err) {
+        if (err) {
+          var response = {status: 'falha', resultado: err};
+          res.json(response);
+        } else {
+          var response = {status: 'sucesso', resultado: 'Registro incluso com sucesso'};
+          res.json(response);
+        }
+      });
+    }
+  });
+});
 
